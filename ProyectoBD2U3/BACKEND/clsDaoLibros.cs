b3eae868350e90293ae451789a3904988b849c33
Karyme Nava <s19120124@alumnos.itsur.edu.mx>
2022-02-16 15:06:15 -0600

    Creación de las carpetas del proyecto y del POJO de libros

---
+++ ProyectoBD2U3/BACKEND/clsDaoLibros.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoBD2U3.BACKEND
+{
+    public class clsDaoLibros
+    {
+    }
+}
---
+++ ProyectoBD2U3/POJOS/clsLibros.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoBD2U3.POJOS
+{
+    public class clsLibros
+    {
+		private int id;
+		private string isbn;
+		private string titulo;
+		private int numEdicion;
+		private int anioPublicacion;
+		private string nombresAutores;
+		private string paisPublicacion;
+		private string sinopsis;
+		private string carrera;
+		private string materia;
+
+		public int ID
+        {
+			get { return id; }
+			set { id = value; }
+        }
+
+		public string ISBN
+        {
+			get { return isbn; }
+			set { isbn = value; }
+        }
+		
+		public string Titulo
+        {
+			get { return titulo; }
+			set { titulo = value; }
+        }
+		
+		public int NumEdicion
+        {
+            get { return numEdicion; }
+            set { numEdicion = value; }
+        }
+
+		public int AnioPublicacion
+        {
+			get { return anioPublicacion; }
+            set { anioPublicacion = value; }
+        }
+		
+		public string NombresAutores
+        {
+			get { return nombresAutores; }
+			set { nombresAutores = value; }
+        }
+
+		public string PaisPublicacion
+        {
+            get { return paisPublicacion; }
+            set { paisPublicacion = value; }
+        }
+
+		public string Sinopsis
+        {
+            get { return sinopsis; }
+            set { sinopsis = value; }
+        }
+
+		public string Carrera
+        {
+            get { return carrera; }
+            set { carrera = value; }
+        }
+
+		public string Materia
+        {
+            get { return materia; }
+			set { materia = value; }
+        }
+	}
+}
